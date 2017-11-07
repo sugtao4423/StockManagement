12664e07ebbe8d432e30ea8e246c5a0908b5e179
sugtao4423 <sugtao4423@gmail.com>
2017-11-08 04:54:28 +0900

    FRONT - first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="ja">
+  <head>
+    <title>Stock Management</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <script src="//code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <link href="./assets/style.css" rel="stylesheet">
+  </head>
+  <body>
+    <nav class="navbar navbar-default">
+        <div class="container">
+            <div class="navbar-header">
+                <a class="navbar-brand">在庫管理</a>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container">
+        <h1>在庫管理</h1>
+    </div>
+
+  </body>
+</html>
